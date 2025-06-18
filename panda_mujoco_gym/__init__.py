7feb169e2d15b10e4fedd3af80597ad6d8f4beb6
lessblue <52413371+lessblue@users.noreply.github.com>
2025-06-18 14:13:25 +0800

    Update __init__.py
    change for add stack env

--- panda_mujoco_gym/__init__.py
+++ panda_mujoco_gym/__init__.py
@@ -16,3 +16,18 @@ for task in ["Slide", "Push", "PickAndPlace"]:
         )
 
         ENV_IDS.append(env_id)
+        
+STACK_TASK_MAX_STEPS = 150  # 为Stack任务定义一个更合理的步长
+
+for reward_type in ["sparse", "dense"]:
+    reward_suffix = "Dense" if reward_type == "dense" else "Sparse"
+    env_id = f"FrankaStack{reward_suffix}-v0"
+
+    register(
+        id=env_id,
+        entry_point=f"panda_mujoco_gym.envs:FrankaStackEnv",
+        kwargs={"reward_type": reward_type},
+        max_episode_steps=STACK_TASK_MAX_STEPS,  # <<< 为Stack定义的150步
+    )
+
+    ENV_IDS.append(env_id)
